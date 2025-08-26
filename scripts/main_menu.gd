7a17027128251f61713e4aedc9428480d4e7db4c
v <153298243+mizureoo@users.noreply.github.com>
2025-08-26 19:27:37 +0800

    Fixed audio

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -9,32 +9,26 @@ extends Control
 @onready var mute_button: CheckButton = $"SettingsPanel/Volume/Mute All/MuteButton"
 @onready var reset_button: Button = $SettingsPanel/Reset
 
-const DEFAULT_MUSIC_VOLUME: float = 0.8
-const DEFAULT_SFX_VOLUME: float = 0.8
-const DEFAULT_MUTE: bool = false
-
 func _ready() -> void:
-	# Panels setup
 	button_container.visible = true
 	settings_panel.visible = false
 	credits_panel.visible = false
-
-	# Connect mute button
 	mute_button.toggled.connect(_on_mute_button_toggled)
-
-	# Connect sliders
 	music_slider.value_changed.connect(_on_music_slider_value_changed)
 	sfx_slider.value_changed.connect(_on_sfx_slider_value_changed)
 
-	# Initialize UI to defaults
-	music_slider.value = DEFAULT_MUSIC_VOLUME
-	sfx_slider.value = DEFAULT_SFX_VOLUME
-	mute_button.button_pressed = DEFAULT_MUTE
+	# Load values from Settings (autoload)
+	music_slider.value = Settings.music_volume
+	sfx_slider.value = Settings.sfx_volume
+	mute_button.button_pressed = Settings.mute
 
-	# Apply defaults to audio
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(DEFAULT_MUSIC_VOLUME))
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(DEFAULT_SFX_VOLUME))
-	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), DEFAULT_MUTE)
+	# Apply them to audio buses
+	_apply_audio_settings()
+
+func _apply_audio_settings() -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(Settings.music_volume))
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(Settings.sfx_volume))
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), Settings.mute)
 
 func _on_play_pressed() -> void:
 	SceneManager.change_scene("res://scenes/level_1.tscn")
@@ -51,21 +45,27 @@ func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 func _on_reset_pressed() -> void:
-	# Reset UI
-	music_slider.value = DEFAULT_MUSIC_VOLUME
-	sfx_slider.value = DEFAULT_SFX_VOLUME
-	mute_button.button_pressed = DEFAULT_MUTE
+	# Reset values in Settings
+	Settings.music_volume = 0.8
+	Settings.sfx_volume = 0.8
+	Settings.mute = false
 
-	# Apply to Audio buses
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(DEFAULT_MUSIC_VOLUME))
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(DEFAULT_SFX_VOLUME))
-	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), DEFAULT_MUTE)
+	# Update UI
+	music_slider.value = Settings.music_volume
+	sfx_slider.value = Settings.sfx_volume
+	mute_button.button_pressed = Settings.mute
 
-func _on_mute_button_toggled(toggled_on: bool) -> void:
-	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), toggled_on)
+	# Apply to audio buses
+	_apply_audio_settings()
 
 func _on_music_slider_value_changed(value: float) -> void:
+	Settings.music_volume = value
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(value))
 
 func _on_sfx_slider_value_changed(value: float) -> void:
+	Settings.sfx_volume = value
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(value))
+
+func _on_mute_button_toggled(toggled_on: bool) -> void:
+	Settings.mute = toggled_on
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), toggled_on)
---
+++ scripts/settings.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var music_volume: float = 0.8
+var sfx_volume: float = 0.8
+var mute: bool = false
