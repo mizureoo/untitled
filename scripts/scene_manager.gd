f5aac583beeea1629c2c551aa41cb5108ad7543a
v <153298243+mizureoo@users.noreply.github.com>
2025-09-09 20:13:54 +0800

    ...

--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -2,8 +2,8 @@ extends CanvasLayer
 
 @onready var transition_animation: AnimationPlayer = $TransitionAnimation
 
-func change_scene(target: String) -> void: 
-	transition_animation.play("fade") 
+func change_scene(target: String, anim_name: String = "fade") -> void:
+	transition_animation.play(anim_name)
 	await transition_animation.animation_finished
 	get_tree().change_scene_to_file(target)
-	transition_animation.play_backwards("fade")
+	transition_animation.play_backwards(anim_name)
