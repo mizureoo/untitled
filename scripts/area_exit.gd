3fa247ed22ea0be627115cd858bdf45e51ec6982
v <153298243+mizureoo@users.noreply.github.com>
2025-09-12 20:05:57 +0800

    ...

--- scripts/area_exit.gd
+++ scripts/area_exit.gd
@@ -1,18 +1,15 @@
 extends Area2D
 
-const FILE_BEGIN = "res://scenes/levels/level_"
-
 func _on_body_entered(body: Node2D):
 	if body is PlayerController:
-		# End level and save stats
+		# End current level
 		GameManager.end_level()
-		GameManager.level += 1
 
-		var next_level_path = FILE_BEGIN + str(GameManager.level) + ".tscn"
-
-		if ResourceLoader.exists(next_level_path):
-			SceneManager.change_scene(next_level_path)
+		if GameManager.level < GameManager.total_levels:
+			# Go to next level
+			GameManager.level += 1
+			SceneManager.change_scene("res://scenes/levels/level_%d.tscn" % GameManager.level)
 			GameManager.start_level(GameManager.level)
 		else:
-			# Completed last level
-			SceneManager.change_scene("res://scenes/GameComplete.tscn")
+			# Last level completed → Game Complete scene
+			SceneManager.change_scene("res://scenes/game_complete.tscn")
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,34 +1,31 @@
 extends Node
 
 var score: int = 0
-var player_health: int = 3
 var level: int = 1
-
-# Stats tracking
-var level_stats: Array = []  # Each element: {"level": 1, "score": 10, "deaths": 2}
-var deaths_in_level: int = 0
+var total_levels: int = 2  # set this to the total number of levels
+var player_health: int = 3
+var level_stats := []  # array to store score/deaths per level
 
 func add_score(amount: int = 1):
 	score += amount
 
 func lose_life():
 	player_health -= 1
-	deaths_in_level += 1
-
-func start_level(level_number: int):
-	level = level_number
-	deaths_in_level = 0
-
-func end_level():
-	level_stats.append({
-		"level": level,
-		"score": score,
-		"deaths": deaths_in_level
-	})
 
 func reset():
 	score = 0
-	player_health = 3
 	level = 1
-	deaths_in_level = 0
+	player_health = 3
 	level_stats.clear()
+
+func start_level(level_number: int):
+	# Called when starting a level to reset per-level stats
+	level = level_number
+	# You can also store deaths per level if needed
+	level_stats.append({"level": level, "score": score, "deaths": 0})
+
+func end_level():
+	# Called when finishing a level
+	# Update stats for the level
+	if level_stats.size() > 0:
+		level_stats[-1]["score"] = score
