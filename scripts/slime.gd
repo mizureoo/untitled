8b7fee7d0a35783a94ef131c569516146f2b085d
v <153298243+mizureoo@users.noreply.github.com>
2025-10-26 00:57:50 +0800

    hmm

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -7,12 +7,21 @@ var direction = 1
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+func _ready():
+	animated_sprite.play("walk")  # Start walking immediately
+
 func _process(delta: float) -> void:
+	# Flip direction when hitting wall/raycast detects obstacle
 	if ray_cast_right.is_colliding():
 		direction = -1
 		animated_sprite.flip_h = false
-	if ray_cast_left.is_colliding():
+	elif ray_cast_left.is_colliding():
 		direction = 1
 		animated_sprite.flip_h = true
-		
+
+	# Move slime
 	position.x += direction * SPEED * delta
+
+	# Always ensure walking animation is playing
+	if animated_sprite.animation != "walk":
+		animated_sprite.play("walk")
